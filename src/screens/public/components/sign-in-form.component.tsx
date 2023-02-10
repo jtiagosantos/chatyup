3d2623f1c2d3bed526921322c531cf6cc3c53bd2
jtiagosantos <tiagosan040@gmail.com>
2023-02-09 21:20:02 -0300

    feat: add react-hook-form and zod to sign-in form

--- src/screens/public/components/sign-in-form.component.tsx
+++ src/screens/public/components/sign-in-form.component.tsx
@@ -1,25 +1,66 @@
 import { Link, Flex } from 'native-base';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 
 import { TextField, Button } from '../../../common/components';
 
 import type { FC } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+
+const signFormSchema = z.object({
+  email: z.string().min(1, { message: 'Campo obrigatório' }),
+  password: z.string().min(1, { message: 'Campo obrigatório' }),
+});
+
+type SignInFormData = z.infer<typeof signFormSchema>;
 
 type SignInFormProps = {
   onChangeToSignUpForm: () => void;
 };
 
 export const SignInForm: FC<SignInFormProps> = ({ onChangeToSignUpForm }) => {
+  const {
+    control,
+    handleSubmit,
+    clearErrors,
+    formState: { errors },
+  } = useForm<SignInFormData>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: zodResolver(signFormSchema),
+  });
+
+  const onSubmit: SubmitHandler<SignInFormData> = ({ email, password }) => {
+    console.log(email, password);
+  };
+
+  const handleChangeToSignUpForm = () => {
+    clearErrors();
+    onChangeToSignUpForm();
+  };
+
   return (
     <Flex>
-      <TextField.Root>
+      <TextField.Root isInvalid={!!errors.email}>
         <TextField.Label>E-mail</TextField.Label>
-        <TextField.Input placeholder="Digite seu e-mail" />
+        <TextField.Input control={control} name="email" placeholder="Digite seu e-mail" />
+        {!!errors.email && <TextField.Error>{errors.email.message}</TextField.Error>}
       </TextField.Root>
-      <TextField.Root mt="16px" mb="32px">
+      <TextField.Root isInvalid={!!errors.password} mt="16px" mb="32px">
         <TextField.Label>Senha</TextField.Label>
-        <TextField.Input placeholder="Digite sua senha" />
+        <TextField.Input
+          control={control}
+          name="password"
+          placeholder="Digite sua senha"
+        />
+        {!!errors.password && (
+          <TextField.Error>{errors.password.message}</TextField.Error>
+        )}
       </TextField.Root>
-      <Button>Entrar</Button>
+      <Button onPress={handleSubmit(onSubmit)}>Entrar</Button>
       <Flex align="center" mt="24px">
         <Link
           _text={{
@@ -32,7 +73,7 @@ export const SignInForm: FC<SignInFormProps> = ({ onChangeToSignUpForm }) => {
       </Flex>
       <Flex align="center" mt="16px">
         <Link
-          onPress={onChangeToSignUpForm}
+          onPress={handleChangeToSignUpForm}
           _text={{
             color: 'violet.800',
             fontSize: '14px',
