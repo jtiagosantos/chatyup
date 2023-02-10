7d3e8c9232042187496a80f48cc9787d060e839a
jtiagosantos <tiagosan040@gmail.com>
2023-02-09 21:18:22 -0300

    feat: restructure text-field to forward component and create text-field error

--- src/common/components/text-field.component.tsx
+++ src/common/components/text-field.component.tsx
@@ -1,7 +1,11 @@
+import { forwardRef, ForwardRefRenderFunction } from 'react';
+import { TextInput } from 'react-native';
 import { Text, Input, FormControl } from 'native-base';
+import { Controller } from 'react-hook-form';
 
-import type { FC } from 'react';
+import type { FC, PropsWithChildren } from 'react';
 import type { IFormControlProps, ITextProps, IInputProps } from 'native-base';
+import type { Control } from 'react-hook-form';
 
 const TextFieldRoot: FC<IFormControlProps> = ({ children, ...props }) => {
   return <FormControl {...props}>{children}</FormControl>;
@@ -17,29 +21,53 @@ const TextFieldLabel: FC<ITextProps> = ({ children, ...props }) => {
   );
 };
 
-const TextFieldInput: FC<IInputProps> = ({ ...props }) => {
+type TextFieldInputProps = IInputProps & {
+  control: Control<any>;
+  name: string;
+};
+
+const TextFieldInputComponent: ForwardRefRenderFunction<
+  typeof TextInput,
+  TextFieldInputProps
+> = ({ control, name, ...props }, ref) => {
   return (
-    <Input
-      bgColor="gray.800"
-      borderColor="transparent"
-      fontSize="16px"
-      color="white"
-      borderWidth="2px"
-      borderRadius="5px"
-      _focus={{
-        borderColor: 'violet.800',
-      }}
-      _invalid={{
-        borderColor: 'red.500',
-      }}
-      placeholderTextColor="gray.500"
-      {...props}
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { value, onChange } }) => (
+        <Input
+          value={value}
+          onChangeText={onChange}
+          ref={ref as any}
+          bgColor="gray.800"
+          borderColor="transparent"
+          fontSize="16px"
+          color="white"
+          borderWidth="2px"
+          borderRadius="5px"
+          _focus={{
+            borderColor: 'violet.800',
+          }}
+          _invalid={{
+            borderColor: 'red.500',
+          }}
+          placeholderTextColor="gray.500"
+          {...props}
+        />
+      )}
     />
   );
+};
+
+const TextFieldInput = forwardRef(TextFieldInputComponent);
+
+const TextFieldError: FC<PropsWithChildren<unknown>> = ({ children }) => {
+  return <FormControl.ErrorMessage>{children}</FormControl.ErrorMessage>;
 };
 
 export const TextField = {
   Root: TextFieldRoot,
   Label: TextFieldLabel,
   Input: TextFieldInput,
+  Error: TextFieldError,
 };
