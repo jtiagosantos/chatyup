2f3e3b8f7daba83075580be5e52a372325231bef
jtiagosantos <tiagosan040@gmail.com>
2023-02-13 21:03:43 -0300

    feat: implement service to create user to database (firestore)

---
+++ src/modules/user/services/create-one-user-to-database.service.ts
@@ -0,0 +1,20 @@
+import firestore from '@react-native-firebase/firestore';
+import { UserMapper } from '../../../infra/firebase/mappers/user.mapper';
+import { ECollections } from '../../../infra/firebase/enums/collections.enum';
+import type { User } from '../types/user.type';
+
+type CreateOneUserToDatabaseInput = Pick<User, 'firstName' | 'username' | 'email'>;
+
+export class CreateOneUserToDatabaseService {
+  public static async execute(user: CreateOneUserToDatabaseInput) {
+    const formattedUser = UserMapper.toFirestore(user);
+
+    await firestore().collection(ECollections.USERS).add({
+      first_name: formattedUser.first_name,
+      username: formattedUser.username,
+      email: formattedUser.email,
+      created_at: formattedUser.created_at,
+      updated_at: formattedUser.updated_at,
+    });
+  }
+}
