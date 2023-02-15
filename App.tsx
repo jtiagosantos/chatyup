070e0a5c6309a84567fa8090b7b640b8719c09d5
jtiagosantos <tiagosan040@gmail.com>
2023-02-15 19:30:01 -0300

    feat: configure custom font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import 'react-native-reanimated';
 import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+} from '@expo-google-fonts/poppins';
 import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/common/routes/app.routes';
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <NavigationContainer>
       <NativeBaseProvider>
