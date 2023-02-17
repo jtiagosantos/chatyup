d2656d3b83a9620aea6f14b05632e960fc89e752
jtiagosantos <tiagosan040@gmail.com>
2023-02-17 09:30:16 -0300

    feat: implement splash screen

--- src/common/routes/app.routes.tsx
+++ src/common/routes/app.routes.tsx
@@ -2,19 +2,32 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //public
 import { AuthScreen } from '../../screens/public/auth/auth.screen';
+import { SplashScreen } from '../../screens/public/splash/splash.screen';
 
 //private
 import { HomeScreen } from '../../screens/private/home/home.screen';
+
+import { useUser } from '../hooks/use-user.hook';
+import { useSplash } from '../hooks/use-splash.hook';
 
 import type { RoutesList } from '../types/routes-list.type';
 
 const Stack = createNativeStackNavigator<RoutesList>();
 
 export const AppRoutes = () => {
+  const { user } = useUser();
+  const { isAppReady } = useSplash();
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="auth" component={AuthScreen} />
-      <Stack.Screen name="home" component={HomeScreen} />
+      {!isAppReady && <Stack.Screen name="splash" component={SplashScreen} />}
+
+      {isAppReady &&
+        (!user ? (
+          <Stack.Screen name="auth" component={AuthScreen} />
+        ) : (
+          <Stack.Screen name="home" component={HomeScreen} />
+        ))}
     </Stack.Navigator>
   );
 };
