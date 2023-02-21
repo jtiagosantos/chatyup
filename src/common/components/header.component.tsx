d9ae014244dce975c747e506809faefa9dbf9d67
jtiagosantos <tiagosan040@gmail.com>
2023-02-20 22:26:26 -0300

    feat: implement header component

---
+++ src/common/components/header.component.tsx
@@ -0,0 +1,39 @@
+import { TouchableOpacity } from 'react-native';
+import { Flex, Text, Factory } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import { CaretLeft } from 'phosphor-react-native';
+
+import type { FC } from 'react';
+
+type HeaderProps = {
+  title: string;
+};
+
+export const Header: FC<HeaderProps> = ({ title }) => {
+  const { goBack } = useNavigation();
+
+  const FactoryTouchableOpacity = Factory(TouchableOpacity);
+
+  return (
+    <Flex
+      w="full"
+      h="46px"
+      flexDir="row"
+      align="center"
+      justify="center"
+      bgColor="gray.800"
+      position="relative">
+      <FactoryTouchableOpacity
+        activeOpacity={0.6}
+        position="absolute"
+        left="24px"
+        top="25%"
+        onPress={goBack}>
+        <CaretLeft size={22} color="#A1A1AA" />
+      </FactoryTouchableOpacity>
+      <Text color="gray.400" fontSize="16px" fontWeight="medium">
+        {title}
+      </Text>
+    </Flex>
+  );
+};
--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,2 +1,3 @@
 export * from './text-field.component';
 export * from './button.component';
+export * from './header.component';
