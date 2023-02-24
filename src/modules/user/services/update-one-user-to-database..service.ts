426c9586fda1c5354afb373f2cf7e03d7eec0f84
jtiagosantos <tiagosan040@gmail.com>
2023-02-24 18:03:20 -0300

    feat: implement services to update user info

---
+++ src/modules/user/services/update-one-user-to-database..service.ts
@@ -0,0 +1,22 @@
+import firestore from '@react-native-firebase/firestore';
+import { ECollections } from '../../../infra/firebase/enums/collections.enum';
+import type { User } from '../types/user.type';
+
+export class UpdateOneUserToDatabaseService {
+  public static async execute({ id, firstName, username, email }: User) {
+    let dataToUpdate = {};
+    if (firstName) {
+      dataToUpdate = { ...dataToUpdate, first_name: firstName };
+    }
+    if (username) {
+      dataToUpdate = { ...dataToUpdate, username };
+    }
+    if (email) {
+      dataToUpdate = { ...dataToUpdate, email };
+    }
+    const dateNow = firestore.Timestamp.now();
+    dataToUpdate = { ...dataToUpdate, updated_at: dateNow };
+
+    await firestore().collection(ECollections.USERS).doc(id).update(dataToUpdate);
+  }
+}
