fc3fff6f1d23a39d6b8023e682874aa17c36f600
jtiagosantos <tiagosan040@gmail.com>
2023-02-26 16:08:20 -0300

    feat: implement edit avatar functionality

---
+++ src/screens/private/my-profile/components/edit-avatar.component.tsx
@@ -0,0 +1,193 @@
+import { useEffect } from 'react';
+import { Image } from 'react-native';
+import { Modal, Flex, Text, useToast } from 'native-base';
+import { Controller, useForm } from 'react-hook-form';
+import * as ImagePicker from 'expo-image-picker';
+
+import { UpdateOneUserToDatabaseService } from '../../../../modules/user/services/update-one-user-to-database..service';
+
+import { Button } from '../../../../common/components';
+
+import { getFileInfo } from '../../../../common/utils/get-file-info.util';
+import { validateFileSize } from '../../../../common/utils/validate-file-size.util';
+import { getFileExtension } from '../../../../common/utils/get-file-extension.util';
+
+import { useUser } from '../../../../common/hooks/use-user.hook';
+import { useLoading } from '../../../../common/hooks/use-loding.hook';
+
+import UserProfileImage from '../../../../common/assets/user-profile.svg';
+
+import type { FC } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+
+import storage from '@react-native-firebase/storage';
+
+const MAX_FILE_SIZE = 2000000; //bytes
+
+type EditAtavarData = {
+  avatar_url: string;
+};
+
+type EditAvatarProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export const EditAvatar: FC<EditAvatarProps> = ({ isOpen, onClose }) => {
+  const { user, updateUserFromState, saveUserToStorage } = useUser();
+  const { isLoading, enableLoading, disableLoading } = useLoading();
+  const {
+    control,
+    handleSubmit,
+    watch,
+    setValue,
+    setError,
+    formState: { errors, defaultValues },
+  } = useForm<EditAtavarData>({
+    defaultValues: {
+      avatar_url: user?.avatarURL,
+    },
+  });
+  const toast = useToast();
+
+  const { avatar_url } = watch();
+  const imagePickerIsDirty = avatar_url !== defaultValues?.avatar_url;
+
+  const handlePickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setValue('avatar_url', result.assets[0].uri);
+    }
+  };
+
+  const onSubmit: SubmitHandler<EditAtavarData> = async ({ avatar_url }) => {
+    enableLoading();
+
+    if (!avatar_url) {
+      setError('avatar_url', { message: 'Arquivo é obrigatório' });
+      disableLoading();
+      return;
+    }
+
+    const fileInfo = await getFileInfo(avatar_url);
+
+    if (fileInfo.size) {
+      const { isValidFile } = validateFileSize(fileInfo.size, MAX_FILE_SIZE);
+
+      if (!isValidFile) {
+        setError('avatar_url', { message: 'Tamanho máximo de arquivo é 2MB' });
+        disableLoading();
+        return;
+      }
+
+      try {
+        const fileExtension = getFileExtension(avatar_url);
+        const reference = storage().ref(`avatar/user-ref-${user!.id}.${fileExtension}`);
+        await reference.putFile(avatar_url);
+        const url = await reference.getDownloadURL();
+
+        await UpdateOneUserToDatabaseService.execute({
+          id: user!.id,
+          avatarURL: url,
+        });
+
+        await saveUserToStorage({
+          avatarURL: url,
+        });
+        updateUserFromState({
+          avatarURL: url,
+        });
+
+        disableLoading();
+
+        toast.closeAll();
+        toast.show({
+          title: 'Foto de perfil atualizada com sucesso',
+          bgColor: 'success.900',
+          duration: 2000,
+          mb: -5,
+        });
+
+        onClose();
+      } catch {
+        disableLoading();
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      setValue('avatar_url', defaultValues?.avatar_url ?? '');
+    };
+  }, []);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      _overlay={{
+        animationPreset: 'slide',
+      }}>
+      <Modal.Content bgColor="gray.900">
+        <Modal.Body>
+          <Controller
+            control={control}
+            name="avatar_url"
+            render={({ field: { value } }) => (
+              <Flex
+                align="center"
+                justify="center"
+                borderWidth="2px"
+                borderColor="violet.800"
+                w="110px"
+                h="110px"
+                mx="auto"
+                borderRadius="55px">
+                <Flex
+                  align="center"
+                  justify="center"
+                  w="100px"
+                  h="100px"
+                  bgColor="gray.700"
+                  borderRadius="50px"
+                  position="relative">
+                  {value ? (
+                    <Image
+                      source={{ uri: value }}
+                      style={{ width: '100%', height: '100%', borderRadius: 50 }}
+                    />
+                  ) : (
+                    <UserProfileImage />
+                  )}
+                </Flex>
+              </Flex>
+            )}
+          />
+          {errors.avatar_url?.message && (
+            <Text color="red.500" textAlign="center" mt="4px">
+              {errors.avatar_url?.message}
+            </Text>
+          )}
+          {imagePickerIsDirty ? (
+            <Button mt="16px" onPress={handleSubmit(onSubmit)} isLoading={isLoading}>
+              Salvar alteração
+            </Button>
+          ) : (
+            <Button mt="16px" onPress={handlePickImage}>
+              Selecionar arquivo
+            </Button>
+          )}
+          <Button variant="ghost" mt="16px" h="30px" onPress={onClose}>
+            Cancelar
+          </Button>
+        </Modal.Body>
+      </Modal.Content>
+    </Modal>
+  );
+};
--- src/screens/private/my-profile/my-profile.screen.tsx
+++ src/screens/private/my-profile/my-profile.screen.tsx
@@ -1,8 +1,10 @@
+import { Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Box, Text, Flex, Button as NBButton } from 'native-base';
+import { Box, Text, Flex, Button as NBButton, useDisclose } from 'native-base';
 import { NotePencil } from 'phosphor-react-native';
 
 import { Button, Output } from '../../../common/components';
+import { EditAvatar } from './components/edit-avatar.component';
 
 import { useUser } from '../../../common/hooks/use-user.hook';
 
@@ -10,6 +12,7 @@ import UserProfileImage from '../../../common/assets/user-profile.svg';
 
 export const MyProfileScreen = () => {
   const { navigate } = useNavigation();
+  const { isOpen, onOpen, onClose } = useDisclose();
   const { user, signOut } = useUser();
 
   const handleNavigationToEditProfileScreen = () => {
@@ -17,58 +20,79 @@ export const MyProfileScreen = () => {
   };
 
   return (
-    <Box flex={1} bgColor="gray.900" p="24px">
-      <Flex flexDir="row" align="center" justify="space-between">
-        <Text color="gray.400" fontSize="16px" fontWeight="medium">
-          Olá, {user?.firstName}!
-        </Text>
-        <Button maxW="110px" h="36px" onPress={handleNavigationToEditProfileScreen}>
-          Editar perfil
+    <>
+      <Box flex={1} bgColor="gray.900" p="24px">
+        <Flex flexDir="row" align="center" justify="space-between">
+          <Text color="gray.400" fontSize="16px" fontWeight="medium">
+            Olá, {user?.firstName}!
+          </Text>
+          <Button maxW="110px" h="36px" onPress={handleNavigationToEditProfileScreen}>
+            Editar perfil
+          </Button>
+        </Flex>
+        <Flex
+          align="center"
+          justify="center"
+          borderWidth="2px"
+          borderColor="violet.800"
+          w="110px"
+          h="110px"
+          mx="auto"
+          mt="32px"
+          borderRadius="55px">
+          <Flex
+            align="center"
+            justify="center"
+            w="100px"
+            h="100px"
+            bgColor="gray.700"
+            borderRadius="50px">
+            <NBButton
+              p={0}
+              w="26px"
+              h="26px"
+              bgColor="violet.800"
+              borderRadius="5px"
+              position="absolute"
+              top={0}
+              right={0}
+              zIndex={3}
+              _pressed={{
+                opacity: 0.6,
+              }}
+              onPress={onOpen}>
+              <NotePencil size={20} color="#fafafa" />
+            </NBButton>
+            {user?.avatarURL ? (
+              <Image
+                source={{ uri: user.avatarURL }}
+                style={{ width: '100%', height: '100%', borderRadius: 50 }}
+              />
+            ) : (
+              <UserProfileImage />
+            )}
+          </Flex>
+        </Flex>
+        <Box mt="24px" mb="16px">
+          <Output.Root>
+            <Output.Label>Primeiro nome</Output.Label>
+            <Output.Value>{user?.firstName}</Output.Value>
+          </Output.Root>
+          <Output.Root mt="16px">
+            <Output.Label>Nome de usuário</Output.Label>
+            <Output.Value>{user?.username}</Output.Value>
+          </Output.Root>
+          <Output.Root mt="16px">
+            <Output.Label>E-mail</Output.Label>
+            <Output.Value>{user?.email}</Output.Value>
+          </Output.Root>
+        </Box>
+        <Button w="30px" variant="ghost" onPress={signOut}>
+          Sair
         </Button>
-      </Flex>
-      <Flex
-        align="center"
-        justify="center"
-        w="100px"
-        h="100px"
-        bgColor="gray.700"
-        borderRadius="50px"
-        mx="auto"
-        mt="32px"
-        position="relative">
-        <NBButton
-          p={0}
-          w="26px"
-          h="26px"
-          bgColor="violet.800"
-          borderRadius="5px"
-          position="absolute"
-          top={0}
-          right={0}
-          _pressed={{
-            opacity: 0.6,
-          }}>
-          <NotePencil size={20} color="#fafafa" />
-        </NBButton>
-        <UserProfileImage />
-      </Flex>
-      <Box mt="24px" mb="16px">
-        <Output.Root>
-          <Output.Label>Primeiro nome</Output.Label>
-          <Output.Value>{user?.firstName}</Output.Value>
-        </Output.Root>
-        <Output.Root mt="16px">
-          <Output.Label>Nome de usuário</Output.Label>
-          <Output.Value>{user?.username}</Output.Value>
-        </Output.Root>
-        <Output.Root mt="16px">
-          <Output.Label>E-mail</Output.Label>
-          <Output.Value>{user?.email}</Output.Value>
-        </Output.Root>
       </Box>
-      <Button w="30px" variant="ghost" onPress={signOut}>
-        Sair
-      </Button>
-    </Box>
+
+      {isOpen && <EditAvatar isOpen={isOpen} onClose={onClose} />}
+    </>
   );
 };
