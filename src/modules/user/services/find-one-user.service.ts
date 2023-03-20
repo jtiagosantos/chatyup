7e88f33f3aea0a6024a0884c3f2e65856efbad73
jtiagosantos <tiagosan040@gmail.com>
2023-03-20 09:34:22 -0300

    refactor: restructure find one user service to supabase

--- src/modules/user/services/find-one-user.service.ts
+++ src/modules/user/services/find-one-user.service.ts
@@ -1,8 +1,7 @@
-import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
-import { UserMapper } from '../../../infra/firebase/mappers/user.mapper';
-import { ECollections } from '../../../infra/firebase/enums/collections.enum';
-import type { UserFromFirestore } from '../../../infra/firebase/mappers/user.mapper';
-import type { User } from '../types/user.type';
+import { supabase } from '../../../infra/supabase/client';
+import { ETables } from '../../../infra/supabase/enums/tables.enum';
+import { UserMapper } from '../../../infra/supabase/mappers/user.mapper';
+import type { UserFromDatabase } from '../../../infra/supabase/mappers/user.mapper';
 
 type FindOneUserInput = {
   email?: string;
@@ -11,32 +10,24 @@ type FindOneUserInput = {
 
 export class FindOneUserService {
   public static async execute({ email, username }: FindOneUserInput) {
-    let response: FirebaseFirestoreTypes.QuerySnapshot<FirebaseFirestoreTypes.DocumentData>;
+    let userFromDatabase: {
+      [x: string]: any;
+    } | null;
 
     if (email) {
-      response = await firestore()
-        .collection(ECollections.USERS)
-        .where('email', '==', email)
-        .get();
+      const response = await supabase.from(ETables.USERS).select('*').eq('email', email);
+      userFromDatabase = response.data?.[0] ?? null;
     } else if (username) {
-      response = await firestore()
-        .collection(ECollections.USERS)
-        .where('username', '==', username)
-        .get();
+      const response = await supabase
+        .from(ETables.USERS)
+        .select('*')
+        .eq('username', username);
+      userFromDatabase = response.data?.[0] ?? null;
     }
 
-    const userExists = !response!.empty;
+    if (!userFromDatabase!) return null;
 
-    if (!userExists) return null;
-
-    const userFromFirestore = response!.docs?.[0]?.data() as UserFromFirestore;
-    const userId = response!.docs?.[0].id;
-
-    const formattedUser = UserMapper.toDomain(userFromFirestore);
-    const user = {
-      ...formattedUser,
-      id: userId,
-    } as Omit<User, 'password'>;
+    const user = UserMapper.toDomain(userFromDatabase as UserFromDatabase);
 
     return user;
   }
