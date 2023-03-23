bf9ecc96f75d09630c8c324ee17def8e41ccacd4
jtiagosantos <tiagosan040@gmail.com>
2023-03-23 19:56:23 -0300

    fix: restructure message mapper

--- src/infra/supabase/mappers/message.mapper.ts
+++ src/infra/supabase/mappers/message.mapper.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { EKindMessage } from '../../../modules/messages/enums/kind-message.enum';
 import type { Message } from '../../../modules/messages/types/message.type';
 
@@ -6,12 +7,16 @@ type MessageToDatabase = Omit<Message, 'id' | 'deletedAt' | 'createdAt'> & {
   roomId: string;
 };
 
-type MessageFromDatabase = {
+export type MessageFromDatabase = {
   id: string;
   kind: keyof typeof EKindMessage;
   content: string;
   deleted_at: Date | null;
   created_at: Date;
+  user: {
+    first_name: string;
+    avatar_url: string;
+  };
 };
 
 export class MessageMapper {
@@ -25,12 +30,18 @@ export class MessageMapper {
   }
 
   public static toDomain(data: MessageFromDatabase): Message {
+    const formattedCreatedAt = dayjs(data.created_at).format('DD/MM/YYYY - hh[h]mm');
+
     return {
       id: data.id,
       kind: data.kind,
       content: data.content,
-      createdAt: data.created_at,
+      createdAt: formattedCreatedAt,
       deletedAt: data.deleted_at,
+      user: {
+        firstName: data.user.first_name,
+        avatarURL: data.user.avatar_url,
+      },
     };
   }
 }
